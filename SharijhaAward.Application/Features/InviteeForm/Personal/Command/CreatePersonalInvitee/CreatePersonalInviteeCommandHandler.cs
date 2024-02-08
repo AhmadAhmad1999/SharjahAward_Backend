64a61807228abe3d25e7108f021c3a07d68666ba
ahmadahmad12123 <ahmadahmad@warshatec.com>
2024-02-08 15:13:17 +0300

    Update Doenload BR Code From Email

--- SharijhaAward.Application/Features/InviteeForm/Personal/Command/CreatePersonalInvitee/CreatePersonalInviteeCommandHandler.cs
+++ SharijhaAward.Application/Features/InviteeForm/Personal/Command/CreatePersonalInvitee/CreatePersonalInviteeCommandHandler.cs
@@ -113,8 +113,8 @@ namespace SharijhaAward.Application.Features.InviteeForm.Personal.Command.Create
                 //        $"&EventName={EventEntity.ArabicName}";
 
                 string DownloadBarCodeImageAPI = isHttps
-                    ? $"https://{_HttpContextAccessor.HttpContext?.Request.Host.Value}/api/Event/DownloadBarCode?BarCodeName={BarCodeImagePath.Split('/').LastOrDefault()}"
-                    : $"http://{_HttpContextAccessor.HttpContext?.Request.Host.Value}/api/Event/DownloadBarCode?BarCodeName={BarCodeImagePath.Split('/').LastOrDefault()}";
+                    ? $"https://{_HttpContextAccessor.HttpContext?.Request.Host.Value}/api/Event/DownloadBarCode?BarCodeName={BarCodeImagePath.Split('\\').LastOrDefault()}"
+                    : $"http://{_HttpContextAccessor.HttpContext?.Request.Host.Value}/api/Event/DownloadBarCode?BarCodeName={BarCodeImagePath.Split('\\').LastOrDefault()}";
 
                 DateTime GregorianDate = new DateTime(EventEntity.StartDate.Year, EventEntity.StartDate.Month,
                     EventEntity.StartDate.Day, EventEntity.StartDate.Hour, EventEntity.StartDate.Minute, EventEntity.StartDate.Second);
@@ -197,8 +197,8 @@ namespace SharijhaAward.Application.Features.InviteeForm.Personal.Command.Create
                 //        $"&EventName={EventEntity.ArabicName}";
 
                 string DownloadBarCodeImageAPI = isHttps
-                    ? $"https://{_HttpContextAccessor.HttpContext?.Request.Host.Value}/api/Event/DownloadBarCode?BarCodeName={BarCodeImagePath.Split('/').LastOrDefault()}"
-                    : $"http://{_HttpContextAccessor.HttpContext?.Request.Host.Value}/api/Event/DownloadBarCode?BarCodeName={BarCodeImagePath.Split('/').LastOrDefault()}";
+                    ? $"https://{_HttpContextAccessor.HttpContext?.Request.Host.Value}/api/Event/DownloadBarCode?BarCodeName={BarCodeImagePath.Split('\\').LastOrDefault()}"
+                    : $"http://{_HttpContextAccessor.HttpContext?.Request.Host.Value}/api/Event/DownloadBarCode?BarCodeName={BarCodeImagePath.Split('\\').LastOrDefault()}";
 
                 DateTime GregorianDate = new DateTime(EventEntity.StartDate.Year, EventEntity.StartDate.Month,
                     EventEntity.StartDate.Day, EventEntity.StartDate.Hour, EventEntity.StartDate.Minute, EventEntity.StartDate.Second);
