237de355634da89860219e508416d5a44ecbdd7d
ahmadahmad12123 <ahmadahmad@warshatec.com>
2024-02-21 14:44:55 +0300

    1. Add Functions TO BaseRepository 2. Add DeleteDynamicAttributeSection API 3. Edit UpdateDynamicAttributeSection API

--- SharijhaAward.Application/Contract/Persistence/IAsyncRepository.cs
+++ SharijhaAward.Application/Contract/Persistence/IAsyncRepository.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +15,24 @@ namespace SharijhaAward.Application.Contract.Persistence
         Task<IReadOnlyList<T>> ListAllAsync();
         Task<IReadOnlyList<T>> GetPagedReponseAsync(int page, int size);
         Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsync(T entity);
         Task DeleteAsync(T entity);
-
+        IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+        T? FirstOrDefault(Expression<Func<T, bool>> predicate);
+        Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        T? LastOrDefault(Expression<Func<T, bool>> predicate);
+        Task<T?> LastOrDefaultAsync(Expression<Func<T, bool>> predicate);
+        IQueryable<TResult> Select<TResult>(Expression<Func<T, TResult>> selector);
+        IQueryable<T> OrderBy<TKey>(Expression<Func<T, TKey>> keySelector);
+        IQueryable<T> OrderByDescending<TKey>(Expression<Func<T, TKey>> keySelector);
+        IQueryable<T> Include(string navigationPropertyPath);
+        IQueryable<T> Include(Expression<Func<T, object>> navigationProperty);
+        IQueryable<T> IncludeThenWhere(Expression<Func<T, object>> navigationProperty,
+            Expression<Func<T, bool>> predicate);
+        IQueryable<T> WhereThenInclude(Expression<Func<T, bool>> predicate,
+            Expression<Func<T, object>> navigationProperty);
+        T? IncludeThenFirstOrDefault(string navigationPropertyPath, Expression<Func<T, bool>> predicate);
+        T? IncludeThenLastOrDefault(string navigationPropertyPath, Expression<Func<T, bool>> predicate);
     }
 }
---
+++ SharijhaAward.Application/Features/DynamicAttributeSectionsFeatures/Commands/DeleteDynamicAttributeSection/DeleteDynamicAttributeSectionCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharijhaAward.Application.Features.DynamicAttributeSectionsFeatures.Commands.DeleteDynamicAttributeSection
+{
+    public class DeleteDynamicAttributeSectionCommand : IRequest<Unit>
+    {
+        public int Id { get; set; }
+    }
+}
