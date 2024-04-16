300d45209243b5599bc8751d5094f9a5aab626da
Shaheen Abou Hassoun <ethshah12@gmail.com>
2024-04-16 10:05:50 +0200

    Add Migration EditTable_Categories_RemoveColumn_Status.

--- SharijhaAward.Persistence/Migrations/20240416080348_EditTable_Categories_RemoveColumn_Status.Designer.cs
+++ SharijhaAward.Persistence/Migrations/20240416080348_EditTable_Categories_RemoveColumn_Status.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SharijhaAward.Persistence;
 
@@ -11,9 +12,11 @@ using SharijhaAward.Persistence;
 namespace SharijhaAward.Persistence.Migrations
 {
     [DbContext(typeof(SharijhaAwardDbContext))]
-    partial class SharijhaAwardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240416080348_EditTable_Categories_RemoveColumn_Status")]
+    partial class EditTable_Categories_RemoveColumn_Status
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
