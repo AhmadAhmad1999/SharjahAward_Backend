ad5c40bfda89b8445728bf82a77a9f83b6640ddc
kasemsa <kasemsa2000@gmail.com>
2024-05-01 15:42:09 +0300

    Edit on Get form By Id

--- SharijhaAward.Application/Features/ProvidedForm/Queries/GetProvidedFormById/GetProvidedFormByIdQueryHandler.cs
+++ SharijhaAward.Application/Features/ProvidedForm/Queries/GetProvidedFormById/GetProvidedFormByIdQueryHandler.cs
@@ -14,26 +14,49 @@ namespace SharijhaAward.Application.Features.ProvidedForm.Queries.GetProvidedFor
         : IRequestHandler<GetProvidedFormByIdQuery, BaseResponse<ProvidedFormDto>>
     {
         private readonly IAsyncRepository<Domain.Entities.ProvidedFormModel.ProvidedForm> _formRepository;
+        private readonly IUserRepository _userRepository;       
         private readonly IMapper _mapper;
 
-        public GetProvidedFormByIdQueryHandler(IAsyncRepository<Domain.Entities.ProvidedFormModel.ProvidedForm> formRepository, IMapper mapper)
+        public GetProvidedFormByIdQueryHandler(IUserRepository userRepository, IAsyncRepository<Domain.Entities.ProvidedFormModel.ProvidedForm> formRepository, IMapper mapper)
         {
             _formRepository = formRepository;
+            _userRepository = userRepository;
             _mapper = mapper;
         }
 
         public async Task<BaseResponse<ProvidedFormDto>> Handle(GetProvidedFormByIdQuery request, CancellationToken cancellationToken)
         {
-            var form = await _formRepository.GetByIdAsync(request.Id);
-            
-            if(form == null)
+            if (request.UserId != null)
             {
-                return new BaseResponse<ProvidedFormDto>("", false, 404);
+                var User = await _userRepository.GetByIdAsync(request.UserId);
+
+                if (User == null)
+                {
+                    return new BaseResponse<ProvidedFormDto>("User Not Found", false, 404);
+                }
+                var form = _formRepository.Where(f => f.Id == request.Id && f.userId == User.Id);
+
+                if (form == null)
+                {
+                    return new BaseResponse<ProvidedFormDto>("Provided Form Not Found", false, 404);
+                }
+                var data = _mapper.Map<ProvidedFormDto>(form);
+
+                return new BaseResponse<ProvidedFormDto>("", true, 200, data);
             }
-            var data = _mapper.Map<ProvidedFormDto>(form);
-           
-            return new BaseResponse<ProvidedFormDto>("", true, 200, data);
-           
+            else
+            {
+                var form = await _formRepository.GetByIdAsync(request.Id);
+
+                if (form == null)
+                {
+                    return new BaseResponse<ProvidedFormDto>("", false, 404);
+                }
+                var data = _mapper.Map<ProvidedFormDto>(form);
+
+                return new BaseResponse<ProvidedFormDto>("", true, 200, data);
+            }
+          
         }
     }
 }
