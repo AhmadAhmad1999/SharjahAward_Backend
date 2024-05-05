a73b829daf6b764855926f1e337879aa524f36f7
kasemsa <kasemsa2000@gmail.com>
2024-05-05 10:50:02 +0300

    Get News By CycleId

---
+++ SharijhaAward.Api/Controllers/AwardSponsorController.cs
@@ -0,0 +1,97 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharijhaAward.Application.Features.Agendas.Commands.DeleteAgenda;
+using SharijhaAward.Application.Features.Agendas.Queries.GetAgendaById;
+using SharijhaAward.Application.Features.AwardSponsorsPage.Commands.CreateAwardSponsor;
+using SharijhaAward.Application.Features.AwardSponsorsPage.Commands.DeleteAwardSponsor;
+using SharijhaAward.Application.Features.AwardSponsorsPage.Commands.UpdateAwardSponsor;
+using SharijhaAward.Application.Features.AwardSponsorsPage.Queries.GetAwardSponsor;
+
+namespace SharijhaAward.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AwardSponsorController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AwardSponsorController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost(Name = "CreateAwardSponsor")]
+        public async Task<IActionResult> CreateAwardSponsor([FromForm] CreateAwardSponsorCommand command)
+        {
+            //get Language from header
+            var language = HttpContext.Request.Headers["lang"];
+
+            command.lang = language!;
+
+            var response = await _mediator.Send(command);
+
+            return response.statusCode switch
+            {
+                200 => Ok(response),
+                404 => NotFound(response),
+                _ => BadRequest(response)
+            };
+        }
+
+        [HttpPut(Name = "UpdateAwardSponser")]
+        public async Task<IActionResult> UpdateAwardSponser([FromForm] UpdateAwardSponsorCommand command)
+        {
+            //get Language from header
+            var language = HttpContext.Request.Headers["lang"];
+
+            command.lang = language!;
+            var response = await _mediator.Send(command);
+
+            return response.statusCode switch
+            {
+                200 => Ok(response),
+                404 => NotFound(response),
+                _ => BadRequest(response)
+            };
+        }
+
+        [HttpDelete("{Id}", Name = "DeleteAwardSponser")]
+        public async Task<IActionResult> DeleteAwardSponser(int Id)
+        {
+            //get Language from header
+            var language = HttpContext.Request.Headers["lang"];
+
+            var response = await _mediator.Send(new DeleteAwardSponsorCommand()
+            {
+                Id = Id,
+                lang = language!
+            });
+            return response.statusCode switch
+            {
+                200 => Ok(response),
+                404 => NotFound(response),
+                _ => BadRequest(response)
+            };
+
+        }
+
+        [HttpGet(Name = "GetAwardSponser")]
+        public async Task<IActionResult> GetAwardSponser()
+        {
+            //get Language from header
+            var Language = HttpContext.Request.Headers["lang"];
+
+            var response = await _mediator.Send(new GetAwardSponsorQuery());
+
+            return response.statusCode switch
+            {
+                200 => Ok(response),
+                404 => NotFound(response),
+                _ => BadRequest(response)
+            };
+
+        }
+
+    }
+}
