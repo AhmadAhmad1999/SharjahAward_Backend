625fa2f6aaa861f5852033012e0701d11de1f3a1
kasemsa <kasemsa2000@gmail.com>
2024-05-06 17:01:37 +0300

    Edit on Reward

---
+++ SharijhaAward.Application/Features/Rewards/Queries/GetRewardById/RewardDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharijhaAward.Application.Features.Rewards.Queries.GetRewardById
+{
+    public class RewardDto
+    {
+        public int Id { get; set; }
+        public int RewardValue { get; set; }
+        public string Rank { get; set; } = string.Empty;
+        public string ArabicRank { get; set; } = string.Empty;
+        public string EnglishRank { get; set; } = string.Empty;
+        public int CategoryId { get; set; }
+    }
+}
