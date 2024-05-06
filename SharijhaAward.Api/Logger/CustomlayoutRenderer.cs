ef904f2d349bf8f2cd9138fa986716dfb5a74c73
Shaheen Abou Hassoun <ethshah12@gmail.com>
2024-05-06 12:24:11 +0300

    1. Add Logger Layout + ActionFilter + Renderer. 2. Edit Database (Add New Table LogUserActions). 3. Add Migration AddTable_LogUserActions.

---
+++ SharijhaAward.Api/Logger/CustomlayoutRenderer.cs
@@ -0,0 +1,36 @@
+﻿using NLog;
+using NLog.LayoutRenderers;
+using System.Diagnostics;
+using System.Text;
+
+namespace SharijhaAward.Api.Logger
+{
+    [LayoutRenderer("Custom-Layout")]
+    public class CustomlayoutRenderer : LayoutRenderer
+    {
+        protected override void Append(StringBuilder builder, LogEventInfo logEvent)
+        {
+            // 1. Date Time.. 
+            // Done in NLog.config File..
+
+            // 2. Activity Id..
+            Guid ActivityId = Trace.CorrelationManager.ActivityId;
+            if (ActivityId != null && ActivityId != Guid.Empty)
+                builder.Append($"ActivityId: {ActivityId} | ");
+
+            // 3. User Name.. 
+            string UserName = Environment.UserName;
+            if (!string.IsNullOrEmpty(UserName))
+                builder.Append($"UserName: {UserName}");
+            else
+                builder.Append(" | UserName: Null");
+
+            // 4. Message
+            // Done in NLog.config File
+
+            // 5. Log Level
+            // Done in NLog.config File
+
+        }
+    }
+}
