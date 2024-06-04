3869cc4adeb198161f7991f651f01eea3190788b
Shaheen Abou Hassoun <ethshah12@gmail.com>
2024-06-04 16:29:04 +0300

    WS-1026 User Table Columns -Look in Details

--- SharijhaAward.Application/Features/UserFeatures/Queries/GetAllUsers/GetAllUsersHandler.cs
+++ SharijhaAward.Application/Features/UserFeatures/Queries/GetAllUsers/GetAllUsersHandler.cs
@@ -3,6 +3,7 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using SharijhaAward.Application.Contract.Persistence;
 using SharijhaAward.Application.Responses;
+using SharijhaAward.Domain.Entities.IdentityModels;
 
 namespace SharijhaAward.Application.Features.UserFeatures.Queries.GetAllUsers
 {
@@ -11,24 +12,48 @@ namespace SharijhaAward.Application.Features.UserFeatures.Queries.GetAllUsers
     {
         private readonly IMapper _Mapper;
         private readonly IUserRepository _UserRepository;
+        private readonly IAsyncRepository<UserRole> _UserRoleRepository;
 
         public GetAllUsersHandler(IMapper Mapper,
-            IUserRepository UserRepository)
+            IUserRepository UserRepository,
+            IAsyncRepository<UserRole> UserRoleRepository)
         {
             _Mapper = Mapper;
             _UserRepository = UserRepository;
+            _UserRoleRepository = UserRoleRepository;
         }
 
         public async Task<BaseResponse<List<GetAllUsersListVM>>> Handle(GetAllUsersQuery Request, CancellationToken cancellationToken)
         {
             string ResponseMessage = string.Empty;
 
-            List<GetAllUsersListVM> Users = _Mapper.Map<List<GetAllUsersListVM>>(await _UserRepository
+            List<UserRole> UserRoleEntities = await _UserRoleRepository
+                .Include(x => x.User!)
+                .Include(x => x.Role!)
+                .Where(x => x.User!.isValidAccount)
+                .ToListAsync();
+
+            List<GetAllUsersListVM> Users = await _UserRepository
                 .Where(x => x.isValidAccount)
                 .OrderByDescending(x => x.CreatedAt)
                 .Skip((Request.page - 1) * Request.pageSize)
                 .Take(Request.pageSize)
-                .ToListAsync());
+                .Select(x => new GetAllUsersListVM()
+                {
+                    Id = x.Id,
+                    Email = x.Email,
+                    PhoneNumber = x.PhoneNumber,
+                    CreatedAt = x.CreatedAt,
+                    UserRoles = UserRoleEntities
+                        .Where(y => y.UserId == x.Id)
+                        .Select(y => new GetAllUserRolesDto()
+                        {
+                            Id = y.RoleId,
+                            Name = Request.lang == "en"
+                                ? y.Role!.EnglishName
+                                : y.Role!.ArabicName
+                        }).ToList()
+                }).ToListAsync();
 
             int TotalCount = await _UserRepository.GetCountAsync(x => x.isValidAccount);
 
--- SharijhaAward.Application/Features/UserFeatures/Queries/GetAllUsers/GetAllUsersListVM.cs
+++ SharijhaAward.Application/Features/UserFeatures/Queries/GetAllUsers/GetAllUsersListVM.cs
@@ -1,13 +1,11 @@
-﻿using SharijhaAward.Domain.Constants;
-
-namespace SharijhaAward.Application.Features.UserFeatures.Queries.GetAllUsers
+﻿namespace SharijhaAward.Application.Features.UserFeatures.Queries.GetAllUsers
 {
     public class GetAllUsersListVM
     {
         public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
-        public Gender Gender { get; set; }
         public string PhoneNumber { get; set; } = null!;
         public DateTime CreatedAt { get; set; }
+        public List<GetAllUserRolesDto> UserRoles { get; set; } = new List<GetAllUserRolesDto>();
     }
 }
