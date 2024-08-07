1e13dea5062d5c441e809804cda7dace32b2a372
Ahmad Ahmad <ahmadahmad@warshatec.com>
2024-08-07 16:23:04 +0300

    1. Add GetAllSubCategoriesWithClasses API in CategoryController. 2. Add Soft Delete Concepts.

---
+++ SharijhaAward.Application/Features/Categories/Queries/GetAllSubCategoriesWithClasses/ClassesDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SharijhaAward.Application.Features.Categories.Queries.GetAllSubCategoriesWithClasses
+{
+    public class ClassesDto
+    {
+        public int Id { get; set; }
+        public string ClassName { get; set; } = null!;
+    }
+}
---
+++ SharijhaAward.Application/Features/Categories/Queries/GetAllSubCategoriesWithClasses/GetAllSubCategoriesWithClassesHandler.cs
@@ -0,0 +1,111 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SharijhaAward.Application.Contract.Persistence;
+using SharijhaAward.Application.Responses;
+using SharijhaAward.Domain.Entities.CategoryEducationalClassModel;
+using SharijhaAward.Domain.Entities.CategoryModel;
+
+namespace SharijhaAward.Application.Features.Categories.Queries.GetAllSubCategoriesWithClasses
+{
+    public class GetAllSubCategoriesWithClassesHandler 
+        : IRequestHandler<GetAllSubCategoriesWithClassesQuery, BaseResponse<List<GetAllSubCategoriesWithClassesListVM>>>
+    {
+        private readonly IAsyncRepository<Category> _CategoryRepository;
+        private readonly IAsyncRepository<CategoryEducationalClass> _CategoryEducationalClassRepository;
+        private readonly IMapper _Mapper;
+        public GetAllSubCategoriesWithClassesHandler(IAsyncRepository<Category> CategoryRepository,
+            IAsyncRepository<CategoryEducationalClass> CategoryEducationalClassRepository,
+            IMapper Mapper)
+        {
+            _CategoryRepository = CategoryRepository;
+            _CategoryEducationalClassRepository = CategoryEducationalClassRepository;
+            _Mapper = Mapper;
+        }
+
+        public async Task<BaseResponse<List<GetAllSubCategoriesWithClassesListVM>>> 
+            Handle(GetAllSubCategoriesWithClassesQuery Request, CancellationToken cancellationToken)
+        {
+            string ResponseMessage = string.Empty;
+
+            if (!string.IsNullOrEmpty(Request.CategoryName))
+            {
+                List<Category> CategoriesEntities = new List<Category>();
+
+                if (Request.lang == "en")
+                {
+                    CategoriesEntities = await _CategoryRepository
+                        .Where(x => x.Cycle.Status == Domain.Constants.Common.Status.Active &&
+                            x.ParentId != null &&
+                            x.EnglishName.ToLower().StartsWith(Request.CategoryName.ToLower()))
+                        .ToListAsync();
+                }
+                else
+                {
+                    CategoriesEntities = await _CategoryRepository
+                        .Where(x => x.Cycle.Status == Domain.Constants.Common.Status.Active &&
+                            x.ParentId != null &&
+                            x.ArabicName.ToLower().StartsWith(Request.CategoryName.ToLower()))
+                        .ToListAsync();
+                }
+
+                List<CategoryEducationalClass> CategoryEducationalClassEntities = await _CategoryEducationalClassRepository
+                    .Where(x => CategoriesEntities.Select(y => y.Id).Contains(x.CategoryId))
+                    .Include(x => x.EducationalClass!)
+                    .ToListAsync();
+
+                List<GetAllSubCategoriesWithClassesListVM> Response = CategoriesEntities
+                    .Select(x => new GetAllSubCategoriesWithClassesListVM()
+                    {
+                        Id = x.Id,
+                        CategoryName = Request.lang == "en"
+                            ? x.EnglishName
+                            : x.ArabicName,
+                        RelatedToClasses = x.RelatedToClasses,
+                        Classes = CategoryEducationalClassEntities
+                            .Select(y => new ClassesDto()
+                            {
+                                Id = y.Id,
+                                ClassName = Request.lang == "en"
+                                    ? y.EducationalClass!.EnglishName
+                                    : y.EducationalClass!.ArabicName
+                            }).ToList()
+                    }).ToList();
+
+                return new BaseResponse<List<GetAllSubCategoriesWithClassesListVM>>(ResponseMessage, true, 200, Response);
+            }
+            else
+            {
+                List<Category> CategoriesEntities = await _CategoryRepository
+                    .Where(x => x.Cycle.Status == Domain.Constants.Common.Status.Active &&
+                        x.ParentId != null)
+                    .ToListAsync();
+
+                List<CategoryEducationalClass> CategoryEducationalClassEntities = await _CategoryEducationalClassRepository
+                    .Where(x => CategoriesEntities.Select(y => y.Id).Contains(x.CategoryId))
+                    .Include(x => x.EducationalClass!)
+                    .ToListAsync();
+
+                List<GetAllSubCategoriesWithClassesListVM> Response = CategoriesEntities
+                    .Select(x => new GetAllSubCategoriesWithClassesListVM()
+                    {
+                        Id = x.Id,
+                        CategoryName = Request.lang == "en"
+                            ? x.EnglishName
+                            : x.ArabicName,
+                        RelatedToClasses = x.RelatedToClasses,
+                        Classes = CategoryEducationalClassEntities
+                            .Select(y => new ClassesDto()
+                            {
+                                Id = y.Id,
+                                ClassName = Request.lang == "en"
+                                    ? y.EducationalClass!.EnglishName
+                                    : y.EducationalClass!.ArabicName
+                            }).ToList()
+                    }).ToList();
+
+                return new BaseResponse<List<GetAllSubCategoriesWithClassesListVM>>(ResponseMessage, true, 200, Response);
+            }
+        }
+    }
+}
